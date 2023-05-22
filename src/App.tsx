791685c8c7d288d05451b458080dfba941c16bbd
HoonDongKang <d159123@naver.com>
2023-05-22 16:45:24 +0900

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { io } from 'socket.io-client'
+import WaitingRoom from './pages/waiting-room/waiting-room'
+import ChatRoom from './pages/chatroom/chatroom'
+
+export const socket = io(`http://localhost:4000/chat`)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<WaitingRoom />} />
+      <Route path="/room/:roomName" element={<ChatRoom />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
